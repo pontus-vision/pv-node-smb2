0094d6070693be4eb73e943c36a1e1dfb44a6f7a
Marek Paulson <marek@paulson.ee>
2022-09-01 08:20:47 +0200

    update tests

--- test/index.ts
+++ test/index.ts
@@ -13,6 +13,15 @@ const main = async () => {
 
     const info = smb2Client.fileInfo(process.env.PV_SMB_URL||'smb://<host or IP>/Windows Path/goes/here/foo.txt')
     console.log (`${JSON.stringify(info)}`)
+    
+    const dataBuffer = Buffer.from("foo", "utf8");
+    const write = smb2Client.writeFileSync(process.env.PV_SMB_URL||'smb://<host or IP>/Windows Path/goes/here/foo.txt', dataBuffer)
+    console.log (write.toString())
+
+    const oldPath = "smb://<host or IP>/Windows Path/goes/here/foo.txt"
+    const newPath = "smb://<host or IP>/Windows Path/goes/here/bar.txt"
+    const rename = smb2Client.renameFileSync(oldPath, newPath)
+    console.log (rename.toString())
 }
 
 
